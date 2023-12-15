7c886cd6c207595dccdc72918fd6114fbb592db5
Kevin Paul Lamadrid <lamadridkevinpaul@gmail.com>
2023-12-15 11:39:03 +0800

    adding docker initial

--- trading_system/settings.py
+++ trading_system/settings.py
@@ -95,10 +95,19 @@ LOGIN_REDIRECT_URL = "api/account/user/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+        "default": {
+         "ENGINE": "django.db.backends.mysql",
+         "NAME": "tradingdb",
+         "USER": "tradinguser",
+         "PASSWORD": "password",
+         "HOST": "db",
+         "PORT": 3306,
+    },
+ 
 }
 
 
